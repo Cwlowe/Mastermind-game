c03375c2a5849f70ca640f49793f9348b6e0ca75
Calvin <calvinwolfelowe@gmail.com>
2016-12-07 14:49:30 -0800

    Added the instruction function

--- Mastermind.py
+++ Mastermind.py
@@ -7,10 +7,21 @@ class mastermind(object):
         self.turns = 0
         self.answer =[]
         self.guess = []
+        self.clue = []
 
     def instructions(self):
-
-        pass
+        print('Rules:')
+        print('1. Computer picks 4 colors\n',
+              '2. The colors are yellow, blue, green, red, white, and orange\n',
+              '3. User also picks 4 colors trying to get close to the computers choices.\n',
+              "4. If the user has colors close to the computer's the computer would put a b or w.\n",
+              '5. b symbolizes black which means a correct color and its in the right place. \n ',
+              '6. w symbolizes white which means there is a correct color. \n',
+              '7. The position of the clue does not correspond to the position it is in. \n',
+              '8. To take a turn type the first initial of the four colors you want to chose \n',
+              '9. Type help to read the instructions again \n',
+              )
+        
 
     def print_board(self):
         pass
@@ -21,12 +32,17 @@ class mastermind(object):
     def inc_turn(self):
         pass
 
+    def simulate_game(self):
+        self.instructions(self)
+
+
+
 game = mastermind()
 
 #after 20 turns
 while(20 == game.turns):
+    game.simulate_game()
 
-    pass
 
 
 
