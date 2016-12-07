7bf952c43c99f7ccf6acdd33ba1eda96a218b57c
Calvin <calvinwolfelowe@gmail.com>
2016-12-07 03:44:25 -0800

    this is an outline

---
+++ Mastermind.py
@@ -0,0 +1,15 @@
+#Mastermind game
+class mastermind(object):
+    def __init__(self):
+        #yellow, blue, green, red, white, orange
+        self.colors = ['y','b','g','r','w','o']
+        self.past = []
+        self.turns = 0
+        self.answer =[]
+        self.
+
+    def print_board(self):
+        pass
+
+    def
+
